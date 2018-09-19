31be473b15840c831285b818e7e5a9852cdb31c6
abhijit sinha <abhijit.sinha@imaginea.com>
2018-09-19 15:11:08 +0530

    Add random name generator and Reformat code

--- src/main/java/com/abhijit/jrg/RandomGenerator.java
+++ src/main/java/com/abhijit/jrg/RandomGenerator.java
@@ -7,6 +7,8 @@ import java.util.stream.IntStream;
 
 public class RandomGenerator {
 
+	private static final String ALPHABETS_WITH_PUNCTUATIONS = "abcdefghijklmnopqrstuvwxyz!,.;:";
+
 	/**
 	 * @return an alphabet between <b>a - z</b>
 	 */
@@ -15,8 +17,7 @@ public class RandomGenerator {
 	}
 
 	/**
-	 * @param characters
-	 *            an array of characters to select from
+	 * @param characters an array of characters to select from
 	 * @return a character from the array of characters
 	 */
 	public static Character character(char... characters) {
@@ -24,8 +25,7 @@ public class RandomGenerator {
 	}
 
 	/**
-	 * @param string
-	 *            the input string from which a character needs to be chosen
+	 * @param string the input string from which a character needs to be chosen
 	 * @return a character from the input string
 	 */
 	public static Character character(String string) {
@@ -33,34 +33,31 @@ public class RandomGenerator {
 	}
 
 	/**
-	 * @param stringLength
-	 *            number of characters in the string
+	 * @param stringLength number of characters in the string
 	 * @return a string with <b>stringLength</b> number of characters
 	 */
 	public static String string(int stringLength) {
 		return IntStream.range(0, stringLength)
-		    .mapToObj(i -> character())
-		    .map(a -> a.toString())
-		    .reduce((a, b) -> a + b)
-		    .get();
+				.mapToObj(i -> character())
+				.map(a -> a.toString())
+				.reduce((a, b) -> a + b)
+				.get();
 	}
 
 	/**
-	 * @param wordCount
-	 *            number of words of size 10 needed
+	 * @param wordCount number of words of size 10 needed
 	 * @return a list of strings with each string size 10 and list size equal to
 	 *         wordCount
 	 */
 	public static List<String> strings(int wordCount) {
 		return IntStream.range(0, wordCount)
-		    .boxed()
-		    .map(i -> string(10))
-		    .collect(Collectors.toList());
+				.boxed()
+				.map(i -> string(10))
+				.collect(Collectors.toList());
 	}
 
 	/**
-	 * @param stringLengths
-	 *            an array of integers specifying the string lengths
+	 * @param stringLengths an array of integers specifying the string lengths
 	 * @return a list of strings with specified string lengths
 	 */
 	public static List<String> strings(int... stringLengths) {
@@ -72,38 +69,53 @@ public class RandomGenerator {
 	}
 
 	/**
-	 * @param string
-	 *            string from where characters are chosen for the random string
-	 * @param size
-	 *            random string size
+	 * @param string string from where characters are chosen for the random string
+	 * @param size   random string size
 	 * @return random string of size: <b>size</b> with characters from <b>string</b>
 	 */
 	public static String string(String string, int size) {
 		return numbers(Integer.class).setMaxlimit(string.length())
-			.setMinlimit(0)
-			.setSize(size)
-			.toList(i -> String.valueOf(string.charAt(i)))
-			.stream()
-			.reduce("", (a, b) -> String.format("%s%s", a, b));
+				.setMinlimit(0)
+				.setSize(size)
+				.toList(i -> String.valueOf(string.charAt(i)))
+				.stream()
+				.reduce("", (a, b) -> String.format("%s%s", a, b));
 	}
 
 	/**
-	 * @param string
-	 *            string from where the characters will be chosen
-	 * @param wordCount
-	 *            number of words generated
+	 * @param string    string from where the characters will be chosen
+	 * @param wordCount number of words generated
 	 * @return list of words generated
 	 */
 	public static List<String> strings(String string, int wordCount) {
 		return IntStream.range(0, wordCount)
-		    .boxed()
-		    .map(i -> string(string, 10))
-		    .collect(Collectors.toList());
+				.boxed()
+				.map(i -> string(string, number(10)))
+				.collect(Collectors.toList());
 	}
 
 	/**
-	 * @param limit
-	 *            the maximum value the random integer can take (minimum is 0)
+	 * @param wordCount number of words in the name
+	 * @return
+	 */
+	public static String name(int wordCount) {
+		return IntStream.range(0, wordCount)
+				.boxed()
+				.map(i -> string(number(10)))
+				.map(str -> str.toUpperCase()
+						.charAt(0) + str.substring(1))
+				.reduce("", (a, b) -> a + " " + b);
+	}
+
+	public static String paragraph(int wordCount) {
+		return IntStream.range(0, wordCount)
+				.boxed()
+				.map(i -> string(ALPHABETS_WITH_PUNCTUATIONS, 10))
+				.reduce("", (a, b) -> a + b);
+	}
+
+	/**
+	 * @param limit the maximum value the random integer can take (minimum is 0)
 	 * @return a positive random integer
 	 */
 	public static Integer number(int limit) {
@@ -111,10 +123,8 @@ public class RandomGenerator {
 	}
 
 	/**
-	 * @param minLimit
-	 *            the minimum value the random integer can take
-	 * @param maxLimit
-	 *            the maximum value the random integer can take
+	 * @param minLimit the minimum value the random integer can take
+	 * @param maxLimit the maximum value the random integer can take
 	 * @return a positive random integer
 	 */
 	public static Integer number(int minLimit, int maxLimit) {
@@ -122,8 +132,7 @@ public class RandomGenerator {
 	}
 
 	/**
-	 * @param limits
-	 *            an array of integer limits
+	 * @param limits an array of integer limits
 	 * @return list of random integers
 	 */
 	public static List<Integer> numbers(int... limits) {
@@ -135,8 +144,7 @@ public class RandomGenerator {
 	}
 
 	/**
-	 * @param limit
-	 *            the maximum value the random number can take (minimum is 0)
+	 * @param limit the maximum value the random number can take (minimum is 0)
 	 * @return a positive random number
 	 */
 	public static Double number(double limit) {
@@ -144,10 +152,8 @@ public class RandomGenerator {
 	}
 
 	/**
-	 * @param minLimit
-	 *            the minimum value the random number can take
-	 * @param maxLimit
-	 *            the maximum value the random number can take
+	 * @param minLimit the minimum value the random number can take
+	 * @param maxLimit the maximum value the random number can take
 	 * @return a positive random number
 	 */
 	public static Double number(double minLimit, double maxLimit) {
@@ -155,8 +161,7 @@ public class RandomGenerator {
 	}
 
 	/**
-	 * @param limits
-	 *            an array of limits
+	 * @param limits an array of limits
 	 * @return list of random numbers
 	 */
 	public static List<Double> numbers(double... limits) {
@@ -186,23 +191,20 @@ public class RandomGenerator {
 		switch (clz.getName()) {
 		case "java.lang.Integer": {
 			return (T) number((Integer) (minLimit == null ? Integer.MIN_VALUE : minLimit),
-				(Integer) (maxLimit == null ? Integer.MAX_VALUE : maxLimit));
+					(Integer) (maxLimit == null ? Integer.MAX_VALUE : maxLimit));
 		}
 		case "java.lang.Double": {
 			return (T) number((Double) (minLimit == null ? Double.MIN_VALUE : minLimit),
-				(Double) (maxLimit == null ? Double.MAX_VALUE : maxLimit));
+					(Double) (maxLimit == null ? Double.MAX_VALUE : maxLimit));
 		}
 		}
 		return null;
 	}
 
 	/**
-	 * @param rows
-	 *            number of rows in the matrix
-	 * @param cols
-	 *            number of columns in the matrix
-	 * @param limit
-	 *            maximum value of each integer entry (minimum is 0)
+	 * @param rows  number of rows in the matrix
+	 * @param cols  number of columns in the matrix
+	 * @param limit maximum value of each integer entry (minimum is 0)
 	 * @return an integer matrix
 	 */
 	public static Integer[][] matrix(int rows, int cols, int limit) {
@@ -214,10 +216,8 @@ public class RandomGenerator {
 	}
 
 	/**
-	 * @param size
-	 *            size of the array
-	 * @param limit
-	 *            maximum value of each entry (minimum is 0)
+	 * @param size  size of the array
+	 * @param limit maximum value of each entry (minimum is 0)
 	 * @return an integer array
 	 */
 	private static Integer[] array(int size, int limit) {
@@ -229,8 +229,7 @@ public class RandomGenerator {
 	}
 
 	/**
-	 * @param list
-	 *            list of objects
+	 * @param list list of objects
 	 * @return randomized list of objects
 	 */
 	public static <T> List<T> randomize(List<T> list) {
